2097bfe9e83d3e3eced93a0a1999dbc205757182
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2022-10-16 18:33:53 -0500

    camera follows player

---
+++ week 9 (camera follows player)/pipe.js
@@ -0,0 +1,9 @@
+var Pipe = function(x, y, height, img) {
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.img = img;
+    this.update = function(){
+        ctx.drawImage(this.img, this.x-orginX/2, this.y, 30, this.height)
+    }
+}
